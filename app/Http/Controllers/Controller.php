8ab1c73b7950993efa9a5f779083a8b87bcaf652
amrianto <saragihamrianto@gmail.com>
2019-05-02 14:57:23 +0700

    Update Controller class to add generateUuid function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Ramsey\Uuid\Uuid;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function generateUuid()
+    {
+        $uuid = Uuid::uuid4();
+        return $uuid->toString();
+    }
 }
